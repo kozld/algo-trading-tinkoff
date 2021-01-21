4bca2aace6f9bca5ee093fcbc8827a19e722cfb3
Denis Kozlov <denis.kozlov94@icloud.com>
2021-01-21 22:18:41 +0300

    update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ docker-compose build --no-cache
 sleep 10
 docker-compose up -d trader
 docker-compose up -d worker
-docker-compose run -d grabber python main.py shell
+docker-compose run -d grabber python grabber.py shell
